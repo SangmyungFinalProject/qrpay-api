7351f74b7ec19bdeb335d3ec2383c059887fcb3c
김지훈 <woowahan@gimjihun-ui-MacBook-Pro.local>
2017-08-10 20:07:28 +0900

    default scheme redirect test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('hello');
+  var testUrl = 'smartbaedal://api?json=%7B%22func%22%3A%22goPage%22%2C%22arg%22%3A%7B%22page%22%3A%22ShopListPagerActivity%22%7D%7D';
+  res.redirect(301, testUrl);
 });
 
 module.exports = router;
