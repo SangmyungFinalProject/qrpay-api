2bd731447f3e833aab220df5c0344a483bb88463
김지훈 <woowahan@gimjihun-ui-MacBook-Pro.local>
2017-08-10 15:11:48 +0900

    test index redirect

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect("smartbaedal://api?json=%7B%22func%22%3A%22goPage%22%2C%22arg%22%3A%7B%22page%22%3A%22event%22%7D%7D");
+  res.redirect(301, "smartbaedal://api?json=%7B%22func%22%3A%22goPage%22%2C%22arg%22%3A%7B%22page%22%3A%22event%22%7D%7D");
 });
 
 module.exports = router;
