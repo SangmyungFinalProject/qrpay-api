581e37a1fbaad264e3bc197f85b7558873c6f192
Ant.Roid <pigberger70@gmail.com>
2017-08-10 20:35:16 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  var testUrl = 'sbca://categoryCode?1';
+  var testUrl = 'sbca://categoryCode';
   res.redirect(301, testUrl);
 });
 
