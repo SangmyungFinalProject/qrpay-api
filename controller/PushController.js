1862518b18a11a3a7dc9b0112fa0ad3e4beddb63
kairo4 <kairo4@naver.com>
2017-09-04 18:41:01 +0900

    pay 수정

--- controller/PushController.js
+++ controller/PushController.js
@@ -1,9 +1,9 @@
 var app = require('./../app');
 var connection = app.connection;
 
-function sendPush(push_token, total_price, callback) {
+function sendPush(token, total_price, callback) {
 
-    callback(null, push_token);
+    callback(null, true);
 }
 
 module.exports = {
