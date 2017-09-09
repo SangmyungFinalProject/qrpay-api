49b2d3e4d3be1036533ad4c55345cd47a45f7b22
김지훈 <woowahan@gimjihun-ui-MacBook-Pro.local>
2017-09-10 00:50:39 +0900

    복호모듈 기능 구현

--- controller/PayController.js
+++ controller/PayController.js
@@ -4,18 +4,22 @@ var PushController = require('./PushController');
 var async = require('async');
 var aes = require('aes-cross');
 
+var errorSet = {
+    dataNull: '1',
+    boundsOver: '2',
+    validOver: '3',
+    timeOver: '4',
+    notExist: '5',
+    syntaxError: '6'
+};
+
 function chargePay(encryptedData, callback) {
 
     var payInfo = decrypt(encryptedData);
 
-    var errorSet = {
-        dataNull: '1',
-        boundsOver: '2',
-        validOver: '3',
-        timeOver: '4',
-        notExist: '5',
-        syntaxError: '6'
-    };
+    if (payInfo === errorSet.timeOver) {
+        return callback(errorSet.timeOver);
+    }
 
     var dt = new Date();
 
@@ -35,11 +39,6 @@ function chargePay(encryptedData, callback) {
             else callback(null);
         },
 
-        /*function (callback) {
-            if(시간 정보 처리해서 0보다 작으면) return callback(errorSet.timeOver);
-            else callback(null);
-        },*/
-
         function (callback) {
             var query = 'select id, bounds, valid_date from card_info where number = ? and cvc = ?';
             connection.query(query, [payInfo.card_number, payInfo.cvc], function (error, rows) {
@@ -56,12 +55,12 @@ function chargePay(encryptedData, callback) {
 
         function (callback) {
             if ((YY_MM - valid_date) > 0) return callback(errorSet.validOver);
-                else callback(null);
+            else callback(null);
         },
 
         function (callback) {
             var query = 'select * from user_card_info where user_id = ? and card_id = ?';
-            connection.query(query, [payInfo.userId, pay_card_id], function(error, rows) {
+            connection.query(query, [payInfo.userId, pay_card_id], function (error, rows) {
                 if (error) return callback(errorSet.syntaxError);
                 else if (rows.length === 0) return callback(errorSet.notExist);
                 else {
@@ -87,14 +86,14 @@ function chargePay(encryptedData, callback) {
 
         function (callback) {
             var query = 'insert into pay_info (total_price, card_id, user_id) values (?, ?, ?)';
-            connection.query(query, [payInfo.total_price, pay_card_id, payInfo.userId], function(error, result) {
+            connection.query(query, [payInfo.total_price, pay_card_id, payInfo.userId], function (error, result) {
                 if (error) return callback(errorSet.syntaxError);
                 else callback(null);
             })
         },
 
         function (callback) {
-            PushController.sendPush(payInfo.userId, payInfo.total_price, function (error, result) {
+            PushController.sendPush(payInfo.deviceToken, payInfo.total_price, function (error, result) {
                 if (error) return callback(error);
                 else callback(null);
             })
@@ -123,14 +122,14 @@ function cancelPay(pay_id, callback) {
         syntaxError: '3'
     };
 
-    connection.query('select * from pay_info where id = ?', pay_id, function(error, rows) {
+    connection.query('select * from pay_info where id = ?', pay_id, function (error, rows) {
         if (rows.length === 0) {
             callback(errorSet.notExist);
         } else {
-            connection.query('update pay_info set cancel = 1 where id = ? and cancel = 0', pay_id, function(error, result) {
+            connection.query('update pay_info set cancel = 1 where id = ? and cancel = 0', pay_id, function (error, result) {
                 if (error) {
                     callback(errorSet.syntaxError);
-                } else if(result.affectedRows <= 0) {
+                } else if (result.affectedRows <= 0) {
                     callback(errorSet.alreadyCancel);
                 } else { //-> 처리가 되면 1 이상, 처리가 되지 않으면 0 이하
                     callback(null);
@@ -142,7 +141,7 @@ function cancelPay(pay_id, callback) {
 
 function payList(userId, callback) {
 
-    connection.query('select * from pay_info where user_id = ?', userId, function(error, rows) {
+    connection.query('select * from pay_info where user_id = ?', userId, function (error, rows) {
         if (error) {
             callback(error);
         } else {
@@ -158,34 +157,27 @@ function decrypt(encryptedData) {
     var iv = new Buffer(new Array(16));
     var key = new Buffer(new Array(32));
 
-    var test = '';
-    test += encryptedData.crypto.card_number;
 
-    console.log(test);
+    var decryptedData = aes.decText(encryptedData.crypto, key, iv);
 
-    var enc = aes.encText(test, key, iv);
-    console.log('enc:%s',enc);
-    var dec = aes.decText(enc,key, iv);
-    console.log('dec:%s',dec);
-
-    // todo time vs pos_time 비교해서 짤라내기
-
-    // if (time - pos_time < 60 * 1000) {
-    //
-    //     return callback(pos_time);
-    // }
+    var data = JSON.parse(decryptedData);
 
     var payInfo = {
-        card_number: encryptedData.crypto.card_number,
-        total_price: encryptedData.total_price,
-        cvc: encryptedData.crypto.cvc,
-        userId: encryptedData.crypto.userId,
-        deviceToken: encryptedData.crypto.deviceToken
-        // time: encryptedData.crypto.time,
-        // pos_time: encryptedData.pos_time
+        card_company: data.cardDisplayModel.cardCompany,
+        card_number: data.cardDisplayModel.cardNumber,
+        cvc: data.cardDisplayModel.cvc,
+        total_price: Number(encryptedData.total_price),
+        userId: data.userId,
+        deviceToken: data.token,
+        time: data.time,
+        pos_time: encryptedData.pos_time
     };
 
-    console.log(payInfo);
+
+    if (Number(payInfo.time) - Number(payInfo.pos_time) < 60 * 1000) {
+
+        return errorSet.timeOver;
+    }
 
     return payInfo;
 }
